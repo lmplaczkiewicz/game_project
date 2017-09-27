08c10c0f22124420199764053b58148916a21e47
lmplaczkiewicz <Frederick.Placzkiewicz@LibertyMutual.com>
2017-09-26 20:02:35 -0400

    add game.js
    game.js console.log showing up on localhost

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,32 +2,16 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+const events = require('./events')
+const game = require('./game')
 
 $(() => {
   setAPIOrigin(location, config)
 })
 
-let startBoard = []
-const playerOne = 'O'
-const playerTwo = 'X'
-const winCombos = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [6, 4, 2]
-]
-
-const cell = document.querySelectorAll('.col-xs-4')
-
-console.log(cell)
-console.log(startBoard)
-console.log(playerOne)
-console.log(playerTwo)
-console.log(winCombos)
+$(() => {
+  events.addHandlers()
+})
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
