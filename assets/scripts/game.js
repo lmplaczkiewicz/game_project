8eb96ee785de41031024b441e1f6bd631dca8364
lmplaczkiewicz <Frederick.Placzkiewicz@LibertyMutual.com>
2017-09-27 21:21:58 -0400

    add response click
    added response on click and if space already filled

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -2,14 +2,23 @@
 
 let player = 0
 
+const checkSquare = function (squareSelected) {
+  if (squareSelected.hasClass('pOne') || squareSelected.hasClass('pTwo')) {
+    console.log('Invalid Space')
+  } else {
+    console.log('We made it')
+  }
+}
 
 const addHandlers = function () {
   $('.col-xs-4').on('click', function (event) {
     let squareSelected = $(this)
     console.log(squareSelected)
+    checkSquare(squareSelected)
   })
 }
 
 module.exports = {
-  addHandlers
+  addHandlers,
+  checkSquare
 }
